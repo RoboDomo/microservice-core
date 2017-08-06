8372249488128dc9a5600843a2903ea4e42ca474
Michael Schwartz <mykesx@gmail.com>
2017-08-06 15:03:10 -0700

    wip

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = {
+exports = {
     MQTT:            process.env.BROWSER ? require('./lib/MQTT') : require('mqtt'),
     APIBase:         require('./lib/APIBase'),
     HostBase:        require('./HostBase'),
