d3d8946ec6f38433858d23eb9f4f764b61bdc744
Michael Schwartz <mykesx@gmail.com>
2017-08-08 12:42:50 -0700

    minor reorg

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 exports = {
-    MQTT:            process.env.BROWSER ? require('./lib/MQTT') : require('mqtt'),
-    APIBase:         require('./lib/APIBase'),
     HostBase:        require('./HostBase'),
     StatefulEmitter: require('./lib/StatefulEmitter'),
 }
